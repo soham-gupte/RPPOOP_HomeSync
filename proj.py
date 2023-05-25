1efdb328ec95f14fb7c656ebeada8f86bbb1d945
Soham Gupte <113963553+soham-gupte@users.noreply.github.com>
2023-05-25 14:11:53 +0530

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,9 +1,14 @@
-from functions import *
+from main import *
 import machine
 import usocket as socket
 import time
 import network
 
-server = Server()
-server.connect_wifi('OnePlus Nord', 'Qwertyuiop')
-server.start_server('soham88gupte@gmail.com')
+server = Server('soham88gupte@gmail.com')
+server.connect_wifi('AndroidAPC371', 'passwordnahimilnar')
+server.start_server()
+
+# Code first starts with connecting to wifi
+# Then waits for login to be successful
+# After successful login, it will restore the previous states of the lights and fan
+# Once operations done, when you press quit, it exits the start_server loop and goes back to check for the login
