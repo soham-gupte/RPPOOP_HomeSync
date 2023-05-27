eec7ef73981735b0077274ee5444430c6e667581
Soham Gupte <113963553+soham-gupte@users.noreply.github.com>
2023-05-27 23:13:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,21 @@ import network
 
 FAN_SPEEDS = [150, 60, 55, 50, 40, 30]
 
-def find_pos(strn, start, char) :
-    while (len(strn)) :
-        if strn[start] == char :
-            return start
-        start += 1
-    return 0 
-
 def get_user(username) :
     with open("database.csv", "r") as file :
         lines = file.readlines()
     for line in lines:
         line = line.strip().split(",")
-        if (line[0] == username) :
+        if (line[9] == username) :
             return line
     
 def update_status(search_email, user_status) :
     with open("database.csv", "r") as file:
         lines = file.readlines()
-
     for i in range(len(lines)):
-        columns = lines[i].strip().split(",")
-        if columns[0] == search_email:
-            # Update the specific value
-            #columns[x] = str(y)  # Update the second-to-last column
-            # Reconstruct the modified line
+        columns = lines[i].split(",")
+        if columns[9] == search_email:
             updated_line = ",".join(user_status)
-            # Replace the line in the list
             lines[i] = updated_line
             break
 
@@ -42,19 +30,11 @@ def update_status(search_email, user_status) :
 
 class HomeSync:
     
-    #def __init__(self) :
-    #    print("HomeSync was established!")
-    
     def __init__(self, username) :
         self.username = username
         self.user_stats = get_user(self.username)
-    #self.LED0 = machine.Pin(12, machine.Pin.OUT)
-        #self.LED1 = machine.Pin(13, machine.Pin.OUT)
-        #self.LED2 = machine.Pin(26, machine.Pin.OUT)
-        #self.LED3 = machine.Pin(25, machine.Pin.OUT)
-    #self.FAN_pin = machine.Pin(5)
-    #self.FAN = machine.PWM(self.FAN_pin, freq=50)
-
+        print(self.user_stats)
+        
 class Bedroom(HomeSync) :
     
     def __init__(self, username) :
@@ -67,7 +47,6 @@ class Bedroom(HomeSync) :
         self.LED3.value(x)
         z[7] = str(x)
         print(z)
-        #self.update_stats(self.user_stats, z)
 
 class DiningRoom(HomeSync) :
     
@@ -81,8 +60,7 @@ class DiningRoom(HomeSync) :
         self.LED2.value(x)
         z[6] = str(x)
         print(z)
-        #self.update_stats(self.user_stats, z)
-
+        
 class Kitchen(HomeSync) :
     
     def __init__(self, username) :
@@ -95,7 +73,6 @@ class Kitchen(HomeSync) :
         self.LED1.value(x)
         z[5] = str(x)
         print(z)
-        #self.update_stats(self.user_stats, z)
     
 class LivingRoom(HomeSync) :
     
@@ -112,22 +89,17 @@ class LivingRoom(HomeSync) :
         self.LED0.value(x)
         z[4] = str(x)
         print(z)
-        #self.update_stats(self.user_stats, z)
         
     def set_fan_speed(self, x, z) :
         y = FAN_SPEEDS[x]
         self.FAN.duty(y)
         z[8] = str(y)
-        #self.update_stats(self.user_stats, z)
-    
 
 class Server:
     
-    def __init__(self, username, password):
+    def __init__(self):
         self.timeout = 0
         self.wifi = network.WLAN(network.STA_IF)
-        self.username = username
-        self.password = password
         
     def connect_wifi(self, ssid, password):
         self.wifi.active(False)
@@ -160,15 +132,7 @@ class Server:
         with open('database.csv', 'a') as file:
             file.write(f"\n{request[0]},{request[1]},{request[2]},{request[3]},0,0,0,0,150,{request[4]}")
             
-    def start_server(self) :
-        
-        #user_stats = get_user(self.username)
-        self.user_stats = get_user(self.username)
-        self.homesync = HomeSync(self.username)
-        self.living_room = LivingRoom(self.username)
-        self.kitchen = Kitchen(self.username)
-        self.dining_room = DiningRoom(self.username)
-        self.bedroom = Bedroom(self.username)
+    def init_server(self) :
         
         # Initializing socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET - Internet Socket, SOCK_STREAM - TCP protocol
@@ -179,10 +143,15 @@ class Server:
         s.bind((host, port))
 
         s.listen(5)  # It will handle a maximum of 5 clients at a time
+        
+        self.s = s
         print("Server started...")
+    
+    def start_server(self) :
         
         while True :
-            connection_socket, address = s.accept()
+            self.username = "USERNAME"
+            connection_socket, address = self.s.accept()
             request = connection_socket.recv(1024)  # Storing response coming from the client
             print("Content", request)  # Printing response
             request = str(request)
@@ -199,15 +168,23 @@ class Server:
                 print("Valid login!")
                 connection_socket.send(bytes("1", "utf-8"))
                 connection_socket.close()
+                self.username = request[0]
                 break
             else :
                 print("Invalid login!")
                 connection_socket.send(bytes("0", "utf-8"))
                 connection_socket.close()
         
+        self.user_stats = get_user(self.username)
+        self.homesync = HomeSync(self.username)
+        self.living_room = LivingRoom(self.username)
+        self.kitchen = Kitchen(self.username)
+        self.dining_room = DiningRoom(self.username)
+        self.bedroom = Bedroom(self.username)
+            
         while True :
             
-            connection_socket, address = s.accept()  # Storing Conn_socket & address of a new client connected
+            connection_socket, address = self.s.accept()  # Storing Conn_socket & address of a new client connected
             print("Got a connection from", address)
             request = connection_socket.recv(1024)  # Storing response coming from the client
             print("Content", request)  # Printing response
@@ -223,18 +200,23 @@ class Server:
             
             if led_on0 == 6:
                 self.living_room.toggle_light(self.user_stats)
-            if led_on1 == 6:
+            elif led_on1 == 6:
                 self.kitchen.toggle_light(self.user_stats)
-            if led_on2 == 6:
+            elif led_on2 == 6:
                 self.dining_room.toggle_light(self.user_stats)
-            if led_on3 == 6:
+            elif led_on3 == 6:
                 self.bedroom.toggle_light(self.user_stats)
-            if fan_stat == 6:
+            elif fan_stat == 6:
                 self.living_room.set_fan_speed(int(request[10]), self.user_stats)
                 
             connection_socket.close()
             
             if exit_status == 6:
+                update_status(self.username, self.user_stats)
                 break
-        print(self.user_stats)
-        update_status(self.username, self.user_stats)
+            
+        self.living_room.LED0.value(0)
+        self.kitchen.LED1.value(0)
+        self.dining_room.LED2.value(0)
+        self.bedroom.LED3.value(0)
+        self.living_room.FAN.duty(150)
