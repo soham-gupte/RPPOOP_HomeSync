7bf115129d6f50063e1aef26fb968703de1c784e
Pranjali <pranjali30pj@gmail.com>
2023-10-21 10:05:19 +0530

    Added custom themes button

--- home.py
+++ home.py
@@ -1,16 +1,37 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang.builder import Builder
-from kivy.animation import Animation    
+from kivy.animation import Animation
 from kivy.properties import ObjectProperty, BooleanProperty, NumericProperty
 from kivy.metrics import dp
-from kivy.graphics import Canvas
-from kivy.graphics import Rectangle
+import json
+from kivy.properties import StringProperty
+from kivy.uix.spinner import Spinner
+from kivy.factory import Factory
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+def open_theme_spinner_dropdown(self):
+        # This method will open the dropdown for the theme spinner
+        theme_spinner = self.ids.theme_spinner
+        theme_spinner.is_open = True
+
+class MySpinnerOption(Button):
+    pass
+
+class MySpinner(Spinner):
+    dropdown_cls = "MySpinnerDropdown"
+
+class MySpinnerDropdown(DropDown):
+    pass
 
 class HomeScreen(Screen):
     Builder.load_file('home.kv')
     Builder.load_file('myprofile.kv')
     menu_active = BooleanProperty(False)
     menu_width = NumericProperty(dp(250))
+
+    theme = StringProperty("Default")
 
     kitchen_light_switch = ObjectProperty(None)
     living_room_light_switch = ObjectProperty(None)
@@ -19,8 +40,58 @@ class HomeScreen(Screen):
     living_room_fan_switch = ObjectProperty(None)
     light_switch = ObjectProperty(None)
 
+    def on_start(self):
+        # Load the initial theme from themes.json
+        with open("themes.json", "r") as theme_file:
+            themes = json.load(theme_file)
+        initial_theme = themes.get("default")  # You can set your default theme here
+        if initial_theme:
+            # Apply the initial theme to the HomeScreen
+            home_screen = self.root.get_screen("home")
+            home_screen.theme = "default"  # Set the initial theme name
+            home_screen.load_theme()
+
+    def load_theme(self):
+        # Define themes in a dictionary with properties
+        themes = {
+        "Default": {
+            "background_color": [1, 1, 1, 1],
+            "text_color": [0, 0, 0, 1],
+            "button_color": [0.65, 0.68, 0.96, 1],
+            "button_size_hint": [None, None],
+            "button_font_size": dp(20),
+            "button_width": dp(150),
+            "button_height": dp(50),
+            "button_background_color": [0.65, 0.68, 0.96, 0.8],
+            "button_color": [1, 1, 1, 1]
+        },
+        "Dark": {
+            "background_color": [0, 0, 0, 1],
+            "text_color": [1, 1, 1, 1],
+            "button_color": [0.2, 0.2, 0.2, 1],
+        },
+        "Green": {
+            "background_color": [0.1, 0.3, 0.1, 1],
+            "text_color": [1, 1, 1, 1],
+            "button_color": [0.1, 0.5, 0.1, 1],
+        }
+    }
+
+        # Get the theme dictionary based on the selected theme name
+        theme_data = themes.get(self.theme)
+
+        # Apply the theme to widgets
+        if theme_data:
+            self.ids.my_widget.background_color = theme_data["background_color"]
+            self.ids.my_label.color = theme_data["text_color"]
+            self.ids.my_button.background_color = theme_data["button_color"]
+
+    def set_theme(self, theme_name):
+        self.theme = theme_name
+        self.load_theme()
+
     def master_off(self):
-    # Control all light switches to turn them off
+        # Control all light switches to turn them off
         for screen_name in ['kitchen', 'living_room', 'dining_room', 'bedroom']:
             screen = self.manager.get_screen(screen_name)
             light_switch = screen.ids.light_switch
@@ -35,10 +106,8 @@ class HomeScreen(Screen):
 
     def toggle_menu(self):
         self.menu_active = not self.menu_active
-        #menu_width = self.width
         if self.menu_active:
-            Animation(width= 0, d=0.2).start(self.ids.menu)
-            #self.ids.menu.pos = (0, 0.5)
+            Animation(width=0, d=0.2).start(self.ids.menu)
         else:
             Animation(width=self.menu_width, d=0.2).start(self.ids.menu)
 
@@ -47,22 +116,3 @@ class HomeScreen(Screen):
         home_screen = self.manager.get_screen('home')
         home_screen.menu_active = False
         home_screen.ids.menu.width = self.menu_width
-
-            #self.ids.menu.pos = (-self.menu_width, 0.5)
-    #def toggle_menu(self):
-    #    menu = self.ids.menu
-    #    menu_width = menu.width
-   #     if menu.x == -menu_width:
-            # menu is currently hidden, so slide it in
-    #        Animation(x=0, duration=0.2).start(menu)
-    #    else:
-            # menu is currently shown, so slide it out
-    #        Animation(x=-menu_width, duration=0.2).start(menu)
-
-    
-
-    
-
-
-
-    
